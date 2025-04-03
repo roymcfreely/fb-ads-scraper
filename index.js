d3f3878775502f313626fd1328d3056570cd55f3
Diandra <diandra@Diandras-MacBook-Pro.local>
2025-04-02 23:30:43 -0700

    fix: add chrome dependencies + use system chrome

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ app.post("/scrape", async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: true,
+      executablePath: "/usr/bin/google-chrome",
       args: ["--no-sandbox", "--disable-setuid-sandbox"]
-    });
+      });
 
     const page = await browser.newPage();
     await page.goto("https://www.facebook.com/ads/library/", { waitUntil: "networkidle2" });
